0e63a7127d354bd061933727f43146b7dad2571e
son <son2044@gmail.com>
2025-12-08 15:41:17 +0900

    remove template

---
+++ src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<InputText v-model="text"/>
+		<Button label="Save"/>
+		{{text}}
+	</div>
+</template>
+
+<script setup>
+import { Button, InputText } from 'primevue';
+import { ref } from 'vue';
+
+let text = ref("");
+</script>
+
+<style lang="css" scoped>
+</style>
