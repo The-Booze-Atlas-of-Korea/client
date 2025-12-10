515ebbc9b379f418f462fd696af33c66a8c14f28
son <son2044@gmail.com>
2025-12-10 11:27:41 +0900

    add RegisterPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '@/views/LandingPage.vue'
 import LoginPage from '@/views/LoginPage.vue'
+ import RegisterPage from '@/views/RegisterPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'LoginPage',
       component: LoginPage,
+    },
+    {
+      path: '/register',
+      name: 'RegisterPage',
+      component: RegisterPage,
     },
   ],
 })
