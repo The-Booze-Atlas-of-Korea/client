e66fdaafa7f5bd689af4f681d070d2bc29e88fd3
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2025-12-23 21:27:47 +0900

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
-import { ToastService } from 'primevue'
+import { ToastService, ConfirmationService } from 'primevue'
 
 import 'primeicons/primeicons.css'
 import { createNaverMap } from 'vue3-naver-maps'
@@ -64,6 +64,7 @@ app.use(PrimeVue, {
     },
 });
 app.use(ToastService)
+app.use(ConfirmationService)
 //map VITE_MAP_API_CLIENT_ID
 app.use(createNaverMap, {
   clientId: import.meta.env.VITE_MAP_API_CLIENT_ID, // Required
